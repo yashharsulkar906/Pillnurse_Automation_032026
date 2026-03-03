719d1cdf63bbb9f56d6ebde28b735ccb626ef79d
yashharsulkar906 <yash.h@humingtec.com>
2026-03-03 19:41:38 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         yield page
         browser.close()
