47868d85cbb143eae7f4c0183925404f910277b2
yashharsulkar906 <yash.h@humingtec.com>
2026-03-03 19:50:21 +0530

    Update login_page.py with LoginPage class implementation

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,12 +1,14 @@
 from pages.base_page import BasePage
 
 class LoginPage(BasePage):
-
     USERNAME = "#username"
     PASSWORD = "#password"
     LOGIN_BUTTON = "#loginBtn"
 
     def login(self, username, password):
+        self.wait_for_element(self.USERNAME)
         self.fill(self.USERNAME, username)
+        self.wait_for_element(self.PASSWORD)
         self.fill(self.PASSWORD, password)
+        self.wait_for_element(self.LOGIN_BUTTON)
         self.click(self.LOGIN_BUTTON)
