96bce00324246e5e3b9a0b7978e3abc07dbe4d7d
yashharsulkar906 <yash.h@humingtec.com>
2026-03-04 12:42:01 +0530

    simulation start and stop

--- tests/test_startand_stop_simulation.py
+++ tests/test_startand_stop_simulation.py
@@ -1,25 +1,26 @@
+import re
 from playwright.sync_api import Page, expect
 
 
-def test_simulation_start_stop(page: Page):
-
+def test_example(page: Page) -> None:
     page.goto("https://staging.d2kcjp2bs55j6j.amplifyapp.com/")
-
-    page.get_by_role("button").nth(5).click()
-
-    page.get_by_role("spinbutton").first.fill("02")
-    page.get_by_role("combobox").first.select_option("Minutes")
-
-    page.get_by_role("spinbutton").nth(1).fill("01")
+    page.get_by_role("button", name="Patient List").click()
+    page.get_by_role("button", name="Sync and use this patient").nth(1).click()
+    page.get_by_role("button", name="📊 Live Dashboard").click()
+    page.get_by_role("spinbutton").first.click()
+    page.get_by_role("spinbutton").first.fill("01")
+    page.get_by_role("spinbutton").nth(1).click()
+    page.get_by_role("spinbutton").nth(1).fill("")
+    page.get_by_role("spinbutton").nth(1).click()
+    page.get_by_role("spinbutton").nth(1).click()
+    page.get_by_role("spinbutton").nth(1).click()
+    page.get_by_role("spinbutton").nth(1).fill("05")
     page.get_by_role("combobox").nth(1).select_option("Seconds")
-
+    page.get_by_role("textbox").click()
+    page.get_by_role("textbox").click()
+    page.get_by_role("textbox").click()
     page.get_by_role("button", name="Launch Simulation Loop").click()
-
-    # Assert Stop button becomes visible
-    stop_button = page.get_by_role("button", name="Stop Simulation")
-    expect(stop_button).to_be_visible()
-
-    stop_button.click()
-
-    # Optional: verify launch button appears again
-    expect(page.get_by_role("button", name="Launch Simulation Loop")).to_be_visible()
+    page.get_by_role("button", name="Stop Simulation").click()
+    page.get_by_role("button", name="Patient List").click()
+    page.get_by_role("button", name="Continue with this patient").click()
+    page.get_by_role("button", name="📋 Clinical Records").click()
